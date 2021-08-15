c773e9e5619dd11585b395639174b1298f7f3f09
CaprioZero <tom.armc63@gmail.com>
2021-08-15 16:16:32 +0700

    Excercise 1.2 done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,12 +7,23 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
+    </div>
+    
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
+      <Part part1={props.part1} exercises1={props.exercises1}/>
+      <Part part2={props.part2} exercises2={props.exercises2}/>
+      <Part part3={props.part3} exercises3={props.exercises3}/>
     </div>
   )
 }
@@ -37,9 +48,7 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </>
   )
