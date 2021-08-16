fda7c7b41d7be069293bbecc355aadb0f29131e5
CaprioZero <tom.armc63@gmail.com>
2021-08-17 00:00:35 +0700

    Excercise 1.8 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-const Header = ({textHeader}) => (
+const Header = ({ textHeader }) => (
   <h1>{textHeader}</h1>
 )
 
@@ -20,12 +20,25 @@ const Text = (props) => {
   )
 }
 
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad
+  const average = (props.bad * (-1) + props.good) / total
+  const positive = props.good * (100 / total)
+  return (
+    <div>
+      <Text text="all" number={total} />
+      <Text text="average" number={average} />
+      <Text text="positive" number={positive} />
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
+
 
   const handleClickGood = () => {
     setGood(good + 1)
@@ -39,22 +52,18 @@ const App = () => {
     setBad(bad + 1)
   }
 
-  const total = bad + good + neutral;
-
   return (
     <>
       <Header textHeader="give feedback" />
       <Button handleClick={handleClickGood} text='good' />
       <Button handleClick={handleClickNeutral} text='neutral' />
       <Button handleClick={handleClickBad} text='bad' />
-      <br/>
+      <br />
       <Header textHeader="statistics" />
-      <Text text="good" number={good}/>
-      <Text text="neutral" number={neutral}/>
-      <Text text="bad" number={bad}/>
-      <Text text="all" number={total}/>
-      <Text text="average" number={(bad*(-1) + good) /total}/>
-      <Text text="positive" number={good * (100/total)}/>
+      <Text text="good" number={good} />
+      <Text text="neutral" number={neutral} />
+      <Text text="bad" number={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
