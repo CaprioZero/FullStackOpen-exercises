f50d83aef94d24de01aaa9e89f4f958ffdee7c30
CaprioZero <tom.armc63@gmail.com>
2021-08-19 00:03:03 +0700

    Exercise 2.3 done with alternative sum function

--- part2/courseinfo-redux/src/index.js
+++ part2/courseinfo-redux/src/index.js
@@ -7,12 +7,22 @@ const Header = ({ course }) => {
   )
 }
 
-const Total = ({ course }) => {
-  let sum = 0
-  course.parts.map(part =>
-    sum += part.exercises
-  )
+// const Total = ({ course }) => {
+//   let sum = 0
+//   course.parts.map(part =>
+//     sum += part.exercises
+//   )
+//   return (
+//     <p><b>total of {sum} exercises</b></p>
+//   )
+// }
 
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((total, arr) => {
+    // return the sum with previous value
+    return total + arr.exercises;
+    // set initial value as 0
+  }, 0);
   return (
     <p><b>total of {sum} exercises</b></p>
   )
