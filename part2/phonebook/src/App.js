5a5422de5735c748cffd5d245ab28f0c3c70b285
CaprioZero <tom.armc63@gmail.com>
2021-08-23 18:02:44 +0700

    Exercise 2.9 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -16,21 +20,34 @@ const App = () => {
     persons.filter(person => person.name === newName).length > 0 ? alert(`${newName} is already added to phonebook`) : setPersons(persons.concat(personObject))
     setNewName('')
     setNewNumber('')
+    // setNewFilter('')
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    // console.log(event.target.value)
+    setNewFilter(event.target.value)
+    const regex = new RegExp(newFilter, 'i');
+    const filteredPersons = () => persons.filter(person => person.name.match(regex))
+    setPersons(filteredPersons)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newFilter} onChange={handleFilterChange} />
+      </div>
+      <h3>Add a new</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -43,7 +60,7 @@ const App = () => {
         </div>
       </form>
       {/*<div>debug: {newName}</div>*/}
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <ul>
         {persons.map(person =>
           <li key={person.name}>
