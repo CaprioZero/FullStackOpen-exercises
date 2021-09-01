02ade33056bff4643c0c33ee2fe1f5dc416a1f68
CaprioZero <tom.armc63@gmail.com>
2021-09-01 17:11:50 +0700

    Exercise 3.3 get individual done

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -34,6 +34,17 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${currentDate}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.statusMessage = "Can't find person";
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
