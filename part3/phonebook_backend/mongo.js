1876a9d1fb75d24f19fb080c5d236f867e80951d
CaprioZero <tom.armc63@gmail.com>
2021-09-03 23:33:18 +0700

    Exercise 3.12 create MongoDB done

---
+++ part3/phonebook_backend/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const pName = process.argv[3]
+const pNumber = process.argv[4]
+
+const url =
+    `mongodb+srv://fullstackopen:${password}@cluster0.6w2c9.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length < 4) {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    const person = new Person({
+        name: `${pName}`,
+        number: `${pNumber}`,
+    })
+
+    person.save().then(result => {
+        console.log('person saved!')
+        mongoose.connection.close()
+    })
+}
