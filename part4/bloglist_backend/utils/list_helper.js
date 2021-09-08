e8e180cb7645ff58a178f5f9ecc64142da0644f8
CaprioZero <tom.armc63@gmail.com>
2021-09-08 23:25:58 +0700

    Exercise 4.5 new favoriteBlog function done

--- part4/bloglist_backend/utils/list_helper.js
+++ part4/bloglist_backend/utils/list_helper.js
@@ -7,8 +7,20 @@ const totalLikes = (blogs) => {
     ? 0
     : blogs.reduce((sum, blog) => sum + blog.likes, 0)
   }
+
+const favoriteBlog = (blogs) => {
+    const maxLikes = Math.max(...blogs.map(blog => blog.likes), 0)
+    return blogs.length === 0
+    ? {}
+    : {
+        title: blogs.find(blog => blog.likes === maxLikes).title,
+        author: blogs.find(blog => blog.likes === maxLikes).author,
+        likes: maxLikes
+    }
+  }
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
