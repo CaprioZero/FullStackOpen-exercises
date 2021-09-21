e1ae7594010c19fa9d504c445af44930559a8bb7
CaprioZero <tom.armc63@gmail.com>
2021-09-22 00:58:02 +0700

    Exercise 4.19 add new blogs with POST request using valid token done

--- part4/bloglist_backend/controllers/blogs.js
+++ part4/bloglist_backend/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -16,9 +17,22 @@ blogsRouter.get('/:id', async (request, response) => {
     }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const user = await User.findById(body.userId)
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     if (!body.likes) {
         body.likes = 0
@@ -34,7 +48,7 @@ blogsRouter.post('/', async (request, response) => {
 
     const savedBlog = await blog.save()
     response.json(savedBlog.toJSON())
-    
+
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
 })
