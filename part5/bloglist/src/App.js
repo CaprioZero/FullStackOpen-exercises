df3cc64f61abdcba0fd959a35f61e12423e4bb24
CaprioZero <tom.armc63@gmail.com>
2021-09-26 11:05:47 +0700

    Exercise 5.9 descending sort base on likes done

--- part5/bloglist/src/App.js
+++ part5/bloglist/src/App.js
@@ -111,7 +111,7 @@ const App = () => {
         <Togglable buttonLabel="Create new blog" ref={blogFormRef}>
           <BlogForm blogData={addBlog} />
         </Togglable>
-        {blogs.map(blog =>
+        {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
          <Blog key={blog.id} blog={blog} setUpdateState={setUpdateState} />
         )}
       </>
