abbc6400a41bc928097ac53494a080db40559a16
CaprioZero <tom.armc63@gmail.com>
2021-09-26 07:11:26 +0700

    Exercise 5.7 implement show/hide blog information done

--- part5/bloglist/src/components/Blog.js
+++ part5/bloglist/src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    <p>"{blog.title}" by "{blog.author}"</p>
-  </div>  
-)
+import React , { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const buttonLabel = visible ? 'Hide' : 'View'
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <p>"{blog.title}" by "{blog.author}"{'\u00A0'}<button onClick={toggleVisibility}>{buttonLabel}</button></p>
+      </div>
+      <div style={showWhenVisible}>
+        <p>Url: {blog.url}</p>
+        <p>Likes: {blog.likes}{'\u00A0'}<button>Like</button></p>
+        <p>Poster: {blog.user.name}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
