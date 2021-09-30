8f9f521fee412c39fc4ded499d548eca0264180c
CaprioZero <tom.armc63@gmail.com>
2021-10-01 01:16:57 +0700

    Exercise 5.22 check blogs are ordered according to likes done

--- part5/bloglist/cypress/integration/blog_app.spec.js
+++ part5/bloglist/cypress/integration/blog_app.spec.js
@@ -1,3 +1,4 @@
+//like test can be flaky sometimes
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
@@ -81,6 +82,7 @@ describe('Blog app', function() {
 
         cy.get('html').should('not.contain', 'like test')
       })
+
       describe('users who doesn\'t create the blog can\'t delete it', function () {
         beforeEach(function () {
           cy.get('button[id="logout-button"]').click()
@@ -90,6 +92,41 @@ describe('Blog app', function() {
         it('check delete button doesn\'t exist', function () {
           cy.contains('like test').contains('View').click()
           cy.get('html').should('not.have.id', 'logout-button')
+        })
+      })
+    })
+
+    describe('and if multiple blog exist', function () {
+      beforeEach(function () {
+        cy.createBlog({ title: 'like test', author: 'test', url: 'https://google.com' })
+        cy.createBlog({ title: 'like test 2', author: 'test', url: 'https://youtube.com' })
+        cy.createBlog({ title: 'like test 3', author: 'test', url: 'https://facebook.com' })
+
+        cy.contains('like test').parent().parent().as('firstBlog')
+        cy.contains('like test 2').parent().parent().as('secondBlog')
+        cy.contains('like test 3').parent().parent().as('thirdBlog')
+      })
+
+      it('check blogs are ordered descendingly according to likes', function () {
+        cy.get('@firstBlog').contains('View').click()
+        cy.get('@secondBlog').contains('View').click()
+        cy.get('@thirdBlog').contains('View').click()
+
+        cy.get('@firstBlog').contains('Like').as('likeButton1')
+        cy.get('@secondBlog').contains('Like').as('likeButton2')
+        cy.get('@thirdBlog').contains('Like').as('likeButton3')
+
+        cy.get('@likeButton3').click()
+        cy.wait(500)
+        cy.get('@likeButton2').click()
+        cy.wait(500)
+        cy.get('@likeButton3').click()
+        cy.wait(500)
+
+        cy.get('.blog').then(blogsMap => {
+          cy.wrap(blogsMap[0]).contains('Likes: 2')
+          cy.wrap(blogsMap[1]).contains('Likes: 1')
+          cy.wrap(blogsMap[2]).contains('Likes: 0')
         })
       })
     })
