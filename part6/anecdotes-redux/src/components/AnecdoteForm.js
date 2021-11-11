4204f1d5da78b8fc4eb50e1ee772b717a28f60c5
CaprioZero <tom.armc63@gmail.com>
2021-11-11 14:02:24 +0700

    Excercise 6.16 modify creation of anecdotes to use async action creators done

--- part6/anecdotes-redux/src/components/AnecdoteForm.js
+++ part6/anecdotes-redux/src/components/AnecdoteForm.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { createNotification } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
 
 const NewAnecdote = () => {
   const dispatch = useDispatch()
@@ -11,8 +10,7 @@ const NewAnecdote = () => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    const newAnecdote = await anecdoteService.createNew(content)
-    dispatch(createAnecdote(newAnecdote))
+    dispatch(createAnecdote(content))
     dispatch(createNotification(`'${content}' added successfully`))
     setTimeout(() => {
       dispatch({ type: 'DELETE_NOTIFICATION' })
