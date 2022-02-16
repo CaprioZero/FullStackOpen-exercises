f8b4cd78d49e39e39a2221046238fa26b1c9133b
CaprioZero <tom.armc63@gmail.com>
2022-02-16 09:43:35 +0700

    Exercise 8.3 implement allAuthors with bookCount query done

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -97,10 +97,17 @@ const typeDefs = gql`
     genres: [String!]
   }
 
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -109,6 +116,12 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => {
+      return books.filter(book => book.author === root.name).length
+    }
   }
 }
 
