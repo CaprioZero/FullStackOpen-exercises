05c9059fe24ecab47b437e51927fbbd87cc4d775
CaprioZero <tom.armc63@gmail.com>
2022-02-18 14:24:24 +0700

    Exercise 8.6 implement addBook mutation done

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require('apollo-server')
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -109,6 +110,15 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -130,6 +140,17 @@ const resolvers = {
   Author: {
     bookCount: (root) => {
       return books.filter(book => book.author === root.name).length
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      if (!authors.find(author => author.name === args.author)) {
+        const author = { name: args.author, id: uuid() }
+        authors = authors.concat(author)
+      }
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
     }
   }
 }
