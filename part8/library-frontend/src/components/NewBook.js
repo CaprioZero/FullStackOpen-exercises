c2be8d5ca66a06fa4fa1026737d044b979841a4f
CaprioZero <tom.armc63@gmail.com>
2022-02-19 22:39:24 +0700

    Exercise 8.10 implement add new books done

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { CREATE_BOOK, ALL_BOOKS, ALL_AUTHORS } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -6,15 +8,30 @@ const NewBook = (props) => {
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const [createBook] = useMutation(CREATE_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+    onError: (error) => {
+      notify(error.graphQLErrors[0].message)
+    }
+  })
 
   if (!props.show) {
     return null
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   const submit = async (event) => {
     event.preventDefault()
 
-    console.log('add book...')
+    createBook({ variables: { title, author, published: parseInt(published, 10), genres } })
 
     setTitle('')
     setPublished('')
@@ -29,42 +46,52 @@ const NewBook = (props) => {
   }
 
   return (
-    <div>
-      <form onSubmit={submit}>
-        <div>
-          title
-          <input
-            value={title}
-            onChange={({ target }) => setTitle(target.value)}
-          />
-        </div>
-        <div>
-          author
-          <input
-            value={author}
-            onChange={({ target }) => setAuthor(target.value)}
-          />
-        </div>
-        <div>
-          published
-          <input
-            type="number"
-            value={published}
-            onChange={({ target }) => setPublished(target.value)}
-          />
-        </div>
-        <div>
-          <input
-            value={genre}
-            onChange={({ target }) => setGenre(target.value)}
-          />
-          <button onClick={addGenre} type="button">
-            add genre
-          </button>
-        </div>
-        <div>genres: {genres.join(' ')}</div>
-        <button type="submit">create book</button>
-      </form>
+    <>
+      <Notify errorMessage={errorMessage} />
+      <div>
+        <form onSubmit={submit}>
+          <div>
+            title
+            <input
+              value={title}
+              onChange={({ target }) => setTitle(target.value)} />
+          </div>
+          <div>
+            author
+            <input
+              value={author}
+              onChange={({ target }) => setAuthor(target.value)} />
+          </div>
+          <div>
+            published
+            <input
+              type="number"
+              value={published}
+              onChange={({ target }) => setPublished(target.value)} />
+          </div>
+          <div>
+            <input
+              value={genre}
+              onChange={({ target }) => setGenre(target.value)} />
+            <button onClick={addGenre} type="button">
+              add genre
+            </button>
+          </div>
+          <div>genres: {genres.join(' ')}</div>
+          <button type="submit">create book</button>
+        </form>
+      </div>
+    </>
+  )
+}
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null
+  }
+  return (
+    <div style={{ color: 'red' }}>
+      {errorMessage}
     </div>
   )
 }
